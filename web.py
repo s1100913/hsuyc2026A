a506e40533a43e56f45f41dce0ac40ca4fa8b037
s1100913 <s1100913@gm.pu.edu.tw>
2026-05-12 10:59:21 +0800

    webhook v01

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response, jsonify
 import requests
 import json
 import urllib3
@@ -57,6 +57,16 @@ def index():
     link += "<a href='/weather'>查詢縣市天氣</a><br>"
     link += "<a href='/rate'>本週新片進DB</a><br>"
     return link
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/rate")
 def rate():
